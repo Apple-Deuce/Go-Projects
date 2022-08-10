2e03a2822cd5eabba4a6f542a6566eb05af5dec7
Apple-Deuce <104399022+Apple-Deuce@users.noreply.github.com>
2022-08-10 17:32:46 -0400

    WIP web scraper using Colly

---
+++ 04_webscraper/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+type item struct {
+	Name   string `json:"name"`
+	Price  string `json:"price"`
+	ImgUrl string `json:"imgurl"`
+}
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("j2store.net"),
+	)
+	c.OnHTML("div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
+		fmt.Println(h.ChildText("h2.product-title"))
+	})
+	c.Visit("https://j2store.net/demo/index.php/shop")
+}
